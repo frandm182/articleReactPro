68cd205eb05380dd72cdf64972661a969ff917fa
Francisco Diaz <franciscojavier.diaz@i3television.es>
2018-01-04 09:21:54 +0100

    Adding jest and tests

--- webpack.config.js
+++ webpack.config.js
@@ -1,16 +1,16 @@
-const path = require('path')
+const path = require('path');
 
 const config = {
-    entry: ['babel-polyfill','./lib/components/Index.js'],
-    output: {
-        path: path.resolve(__dirname, 'public'),
-        filename: 'bundle.js'
-    },
-    module: {
-        rules: [
-            { test: /\.js$/, exclude: /node_modules/,use: 'babel-loader' }
-        ]
-    }
-}
+  entry: ['babel-polyfill','./lib/components/Index.js'],
+  output: {
+    path: path.resolve(__dirname, 'public'),
+    filename: 'bundle.js'
+  },
+  module: {
+    rules: [
+      { test: /\.js$/, exclude: /node_modules/,use: 'babel-loader' }
+    ]
+  }
+};
 
-module.exports = config
+module.exports = config;
